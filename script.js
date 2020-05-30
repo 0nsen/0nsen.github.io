1b6f22933ff8e56b1f3393de7a36194ff1441997
Phuong Gia <sphericalgia@gmail.com>
2020-05-30 11:45:14 +0700

    bugfix

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
     var canvas = document.querySelector("canvas");
     var ctx = canvas.getContext("2d");
     var video = document.querySelector("video");
 
-    //  Draws the current frame of the video every 20 milliseconds
+    //  Draws the current frame of the video every 50 milliseconds
     video.addEventListener('play', function() {
         window.setInterval(function() {
             ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
             LenaJS.filterImage(document.querySelector("#canvas-right"), LenaJS["canny"], document.querySelector("#canvas-left"));
-        }, 200);
+        }, 50);
     }, false);
-}, false);
+}
